54a58c054b42aa89891c16c78e41603777187bdd
Bharti277 <bhartideependra277@gmail.com>
2025-07-07 14:06:58 +0530

    created fs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Signup from "./components/signup/Signup";
 
 function App() {
   return (
     <>
+      <Signup />
       <Counter />
     </>
   );
