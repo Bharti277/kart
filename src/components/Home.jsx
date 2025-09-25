14a56f74da63683fcee67881ff8bc3a7e1c99b52
Bharti277 <bhartideependra277@gmail.com>
2025-09-25 09:16:39 +0530

    added loader

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -40,7 +40,11 @@ const Home = () => {
   };
 
   if (state.isLoading) {
-    return <h2>Loading...</h2>;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-16 h-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   }
 
   return (
